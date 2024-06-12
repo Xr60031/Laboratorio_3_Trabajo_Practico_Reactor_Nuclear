1abc6f190bc6535cbc36566e726798a9cb463ff4
Mathías Ezequiel Latrónico <mathiezelat@gmail.com>
2024-06-12 16:45:27 -0300

    Actualiza clase Computadora para implementar  las interfaces Notificador y Suscriptor

--- src/Controles/Computadora.ts
+++ src/Controles/Computadora.ts
@@ -1,9 +1,12 @@
 import SistemaDeRegulacionTermica from "../SistemaDeRegulacionTermica/ClasesAbstractas/SistemaDeRegulacionTermica";
 import { AlertaTemperatura } from "../Comunicaciones/AlertaTemperatura";
 import { TEMPERATURA_CRITICO } from "../Constantes";
+import Suscriptor from "../Interfaces/Suscriptor";
+import SensorTermico from "../Generadores/Reactor/SensorTermico";
+import Notificador from "../Interfaces/Notificador";
 
-export default class Computadora {
-    private suscribersReguladorTermico!:Array<SistemaDeRegulacionTermica>;
+export default class Computadora implements Notificador, Suscriptor {
+    private suscribersReguladorTermico!: Array<SistemaDeRegulacionTermica>;
     private sistemaRegulacionTermica: SistemaDeRegulacionTermica;
 
     constructor(sistemaDeRegulacionTermica: SistemaDeRegulacionTermica) {
@@ -11,8 +14,20 @@ export default class Computadora {
         this.suscribe(this.sistemaRegulacionTermica);
     }
 
-    public actualizar(temperatura: number): void {
-        this.verificarTemperatura(temperatura);
+    suscribir(suscriptor: Suscriptor): void {
+        throw new Error("Method not implemented.");
+    }
+
+    desuscribir(suscriptor: Suscriptor): void {
+        throw new Error("Method not implemented.");
+    }
+
+    notificar(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    public actualizar(notificador: SensorTermico): void {
+        this.verificarTemperatura(notificador.getTemperatura());
     }
 
     public verificarTemperatura(temperatura: number): void {
@@ -24,26 +39,33 @@ export default class Computadora {
     }
 
     private activarModoEnfriamiento(): void {
-        this.suscribersReguladorTermico.forEach(element => {
+        this.suscribersReguladorTermico.forEach((element) => {
             // Como obtiene la temperatura numérica la computadora?
             // -Eze
-            element.verificadorParaEncender();
+            // element.verificadorParaEncender();
         });
     }
 
     private desactivarModoEnfriamiento(): void {
-        this.suscribersReguladorTermico.forEach(element => {
+        this.suscribersReguladorTermico.forEach((element) => {
             element.apagarSistema();
         });
     }
 
-    public suscribe(sistemaDeRegulacionTermica:SistemaDeRegulacionTermica):void{
+    public suscribe(
+        sistemaDeRegulacionTermica: SistemaDeRegulacionTermica
+    ): void {
         this.suscribersReguladorTermico.push(sistemaDeRegulacionTermica);
     }
 
-    public unsubscribe(sistemaDeRegulacionTermica:SistemaDeRegulacionTermica):void{
-        let iterador=0;
-        while(sistemaDeRegulacionTermica!=this.suscribersReguladorTermico[iterador]){
+    public unsubscribe(
+        sistemaDeRegulacionTermica: SistemaDeRegulacionTermica
+    ): void {
+        let iterador = 0;
+        while (
+            sistemaDeRegulacionTermica !=
+            this.suscribersReguladorTermico[iterador]
+        ) {
             iterador++;
         }
         this.suscribersReguladorTermico.splice(iterador, 1);
