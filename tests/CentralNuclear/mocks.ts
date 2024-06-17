a590ef65aea45a15278afbd400536b8dbae6e0ec
Damian <damianlopez.99@hotmail.com>
2024-06-17 18:41:36 -0300

    Update Central
    El cosntructor de la central conecta los observers.

--- tests/CentralNuclear/mocks.ts
+++ tests/CentralNuclear/mocks.ts
@@ -2,22 +2,24 @@ import Generador from "../../src/Generadores/GeneradorElectrico/Generador";
 import NoHayCombustibleExcepcion from "../../src/Generadores/Reactor/Combustible/ExcepcionesCombustible/NoHayCombustibleExcepcion";
 import Reactor from "../../src/Generadores/Reactor/Reactor";
 import SensorTermico from "../../src/Generadores/Reactor/SensorTermico";
-
+import Suscriptor from "../../src/Interfaces/Suscriptor";
+import SistemaBarrasDeControl from "../../src/SistemaDeRegulacionTermica/SistemaBarrasDeControl/SistemaBarrasDeControl";
 export const ReactorMock : Reactor = {
    generarEnergiaTermica : jest.fn().mockReturnValue(10),
    iniciar : jest.fn(() => {
           throw new NoHayCombustibleExcepcion("No hay combustible para iniciar el reactor.");
         }),
-        getSensorTermico : jest.fn(() => {
-      return SensorMock
-   })
+   getSensorTermico : jest.fn(() => {
+      return SensorMock;
+   }),
+   getSistemaDeRegulacionTermica : jest.fn(() => {return regulaciconMock})
 } as any;
 export const SensorMock : SensorTermico = {
 
    medir : jest.fn((n : number) => {
       return n
-   })
-   
+   }),
+   suscribir : jest.fn((s : Suscriptor) => {return 0})  
 }as any;
 
 export const GeneradorMock : Generador = {
@@ -25,3 +27,4 @@ export const GeneradorMock : Generador = {
       return energiaTermica;
    }
 }as any;
+export const regulaciconMock : SistemaBarrasDeControl = {}as any;
