aa0552a39b5eb22ed6a6746322112323d3be7ad4
Nicolas Galetto <nicomgaletto@gmail.com>
2024-06-18 17:57:31 -0300

    Comunicado de las barras gastadas

--- src/CentralNuclear/DatosEnTodoMomento.ts
+++ src/CentralNuclear/DatosEnTodoMomento.ts
@@ -1,6 +1,7 @@
 export default class DatosEnTodoMomento{
     private _temperatura: number;
     private _energiaProducida: number;
+    private _barrasGastadas: number;
     private _vecesNormal: number;
     private _vecesCritico: number;
     private _vecesApagado: number;
@@ -12,12 +13,24 @@ export default class DatosEnTodoMomento{
     public set temperatura(value: number) {
         this._temperatura = value;
     }
+
     public get energiaProducida(): number {
         return this._energiaProducida;
     }
     public set energiaProducida(value: number) {
         this._energiaProducida = value;
     }
+
+    public get barrasGastadas(): number {
+        return this._barrasGastadas;
+    }
+    public set barrasGastadas(value: number) {
+        this._barrasGastadas = value;
+    }
+    public contarBarra(): void {
+        this._barrasGastadas++;
+    }
+
     public get vecesNormal(): number {
         return this._vecesNormal;
     }
@@ -27,6 +40,7 @@ export default class DatosEnTodoMomento{
     public contarNormal(): void {
         this._vecesNormal++;
     }
+
     public get vecesCritico(): number {
         return this._vecesCritico;
     }
@@ -36,6 +50,7 @@ export default class DatosEnTodoMomento{
     public contarCritico(): void {
         this._vecesCritico++;
     }
+
     public get vecesApagado(): number {
         return this._vecesApagado;
     }
@@ -56,6 +71,7 @@ export default class DatosEnTodoMomento{
     private constructor(){
         this._temperatura = 0;
         this._energiaProducida = 0;
+        this._barrasGastadas = 0;
         this._vecesNormal = 0;
         this._vecesCritico = 0;
         this._vecesApagado = 0;
@@ -64,6 +80,7 @@ export default class DatosEnTodoMomento{
     public toString():string{
         return "Temperatura: " + this.temperatura +
             "\nEnergia producida: " + this.energiaProducida +
+            "\nBarras de control consumidas: " + this.barrasGastadas +
             "\nVeces que el rector estuvo en estado normal: " + this.vecesNormal +
             "\nVeces que el rector estuvo en estado critico: " + this.vecesCritico +
             "\nVeces que el rector debio ser apagado: " + this.vecesApagado;
