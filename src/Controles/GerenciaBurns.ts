1850a3650c96806486c35411397f5838cd2c2c26
Marcos <marcosvelardez40@gmail.com>
2024-06-24 03:42:26 +0000

    grabando observer Burns

--- src/Controles/GerenciaBurns.ts
+++ src/Controles/GerenciaBurns.ts
@@ -4,16 +4,29 @@ import Suscriptor from "../Interfaces/Suscriptor";
 import { Resend } from "resend";
 
 export default class GerenciaBurns implements Suscriptor {
+    private _suscriptoresGerencia: Suscriptor[];
     private _temperaturaReactor: number;
     private _nombre: string;
     private _email: string;
     private _mensaje: string;
 
     constructor(nombre: string, email: string) {
+        this._suscriptoresGerencia = []; 
         this._temperaturaReactor = 0;
         this._nombre = nombre;
         this._email = email;
         this._mensaje = `Señor ${this._nombre}, el reactor ha sido apagado`;
+    }
+
+    public suscribir(suscriptor: Suscriptor): void {
+        this._suscriptoresGerencia.push(suscriptor);
+    }
+
+    public desuscribir(suscriptor: Suscriptor): void {
+        const indice = this._suscriptoresGerencia.indexOf(suscriptor);
+        if (indice > -1) {
+            this._suscriptoresGerencia.splice(indice, 1);
+        }
     }
 
     public actualizar(notificador: SensorTermico): void {
@@ -43,7 +56,14 @@ export default class GerenciaBurns implements Suscriptor {
         catch (error) {
             console.error('Se produjo un error al enviar el Email', error.message);
         }     
+    }
 
+    public getSuscriptores(): Suscriptor[] {
+        return this._suscriptoresGerencia;
+    }
+
+    public setSuscriptores(value: Suscriptor[]) {
+        this._suscriptoresGerencia = value;
     }
     
 }
